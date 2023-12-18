234a4a39a3f4e55a8fa62bca8cc198139cc6de44
nachokhan <ignacio.rigoni@gmail.com>
2023-12-18 16:05:04 -0300

    added different structures

--- pstruc.py
+++ pstruc.py
@@ -1,7 +1,7 @@
 import os
 import json
 import argparse
-import sys
+import yaml
 
 
 def read_gitignore(start_path):
@@ -25,7 +25,7 @@ def read_gitignore(start_path):
     return to_ignore
 
 
-def generate_directory_structure(start_path, output_file):
+def generate_directory_structure(start_path, output_file, output_format):
     if not os.path.exists(start_path):
         print(f"Error: The specified directory '{start_path}' does not exist.")
         return
@@ -49,17 +49,49 @@ def generate_directory_structure(start_path, output_file):
         for filename in filenames:
             current_dir[filename] = None
 
-    with open(output_file, 'w') as json_file:
-        json.dump(structure, json_file, indent=4)
+    if output_format == "json":
+        with open(output_file, 'w') as json_file:
+            json.dump(structure, json_file, indent=4)
+    elif output_format == "yaml":
+        with open(output_file, 'w') as yaml_file:
+            yaml.dump(structure, yaml_file, default_flow_style=False)
+    elif output_format == "txt":
+        with open(output_file, 'w') as txt_file:
+            generate_txt_structure(txt_file, structure, "")
+
+def generate_txt_structure(file, structure, indentation):
+    for key, value in structure.items():
+        if value is None:
+            file.write(indentation + f"- {key}\n")
+        else:
+            file.write(indentation + f"- {key}:\n")
+            generate_txt_structure(file, value, indentation + "  ")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate a directory structure in JSON format.")
+    parser = argparse.ArgumentParser(description="Generate a directory structure in different formats.")
     parser.add_argument("directory", nargs="?", default=os.getcwd(), help="The directory to inspect.")
-    parser.add_argument("-o", "--output", help="The output JSON file name.")
+    parser.add_argument("-o", "--output", help="The output file name.")
+    parser.add_argument("-f", "--format", choices=["json", "yaml", "txt"], default="json", help="The output file format.")
 
     args = parser.parse_args()
 
     start_path = args.directory
     output_file = args.output
+    output_format = args.format
 
+    if output_file is None:
+        # Use the directory name as the default output file name
+        output_file = os.path.basename(start_path)
+
+    # Convert relative path to absolute path if necessary
+    start_path = os.path.abspath(start_path)
+
+    if output_format == "json":
+        output_file += '.json'
+    elif output_format == "yaml":
+        output_file += '.yaml'
+    elif output_format == "txt":
+        output_file += '.txt'
+
+    generate_directory_structure(start_path, output_file, output_format)
