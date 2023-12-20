be668948d7316245d114103e38d1741281aebf81
nachokhan <ignacio.rigoni@gmail.com>
2023-12-20 11:32:21 -0300

    updated tests

--- tests/test_generate_directory_structure.py
+++ tests/test_generate_directory_structure.py
@@ -1,6 +1,6 @@
 import os
 import pytest
-from pstruc._directory_structure import generate_directory_structure
+from pstrucsrc._directory_structure import generate_directory_structure
 
 
 def test_generate_directory_structure_valid_directory():
