3963d1a24a40fde58a4653dbdbe955d687109a25
nachokhan <ignacio.rigoni@gmail.com>
2024-02-02 15:31:03 -0300

    updated example

--- examples/example_ignore_patterns.py
+++ examples/example_ignore_patterns.py
@@ -5,15 +5,20 @@ project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, project_root)
 
 from pstruc import get_project_structure, save_structure_to_file
+from pstruc.file_tools import get_all_ignore_patterns
 
 
 if __name__ == "__main__":
     # Specify the directory path and output format
-    start_path = "/Users/nacho/dev/proyectos/project-structure"
-    output_format = "json"
+    start_path = os.getcwd()
+    output_format = "dict"
 
     # Optional: Specify additional files/folders to ignore
-    ignore_patterns = ["*.png", "*CHANGE*", "CONTRIB*", "*req*", "pstruc.egg-info", ".git", "LICENSE", "dist", "test", "examples", ".github", "*ignore*"]
+    ignore_patterns = get_all_ignore_patterns(
+        start_path,
+        [".gitignore", "/var/lalala/jeje.json"],
+        [".git"]
+    )
 
     try:
         # Generate the directory structure
@@ -21,18 +26,14 @@ if __name__ == "__main__":
             ".",
             output_format,
             ignore_patterns,
-            file_content=["*.py", "README.md"],
+            to_add_content=["*.py", "README.md"],
         )
 
         # Specify the output file name (optional)
         output_file = "output_structure.json"
 
         # Save the directory structure to a file (optional)
-        error = save_structure_to_file(output_file, structure)
-        if error:
-            print(error)  # Handle any errors
-        else:
-            print(f"Directory structure has been generated and saved to '{output_file}'.")
-
+        save_structure_to_file(output_file, structure)
+        print(f"Directory structure has been generated and saved to '{output_file}'.")
     except Exception as e:
         print(e)  # Handle any errors
